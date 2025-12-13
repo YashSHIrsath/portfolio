7e7a660d98cdb9b85bf60ba2f64b9609960490d9
YashSHIrsath <shirsathyash666@gmail.com>
2025-12-13 13:56:36 +0530

    name and middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,6 @@ class AppServiceProvider extends ServiceProvider
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
         
-        // Force HTTPS on Railway
-        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'http') {
-            return redirect()->secure(request()->getRequestUri(), 301);
-        }
-        
         // Share social links with all views (or specific layouts)
         \Illuminate\Support\Facades\View::composer(['components.app-layout', 'home'], function ($view) {
              $view->with('socialLinks', \App\Models\SocialLink::orderBy('sort_order')->get());
